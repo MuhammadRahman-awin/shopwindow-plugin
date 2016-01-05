bdda313abbac1c04e5acb692e2a648f7c524f455
Muhammad Rahman <muhammad.rahman@affiliatewindow.com>
2016-01-05 14:09:41 +0000

    Display horizontal only for shortcode

--- src/ShortCodeHandler.php
+++ src/ShortCodeHandler.php
@@ -14,16 +14,14 @@ class ShortCodeHandler
 
     public function initShortCode($attr)
     {
-        var_dump($attr);
-
-        $layout = $attr['layout'];
-        $layout = empty($layout) ? 'vertical' : $layout;
+        $layout = 'horizontal';
+        $attr['layout'] = $layout;
         $layout = ucfirst($layout);
 
-        echo '
+        return '
         <form name="swFeed" id="swFeed'.$layout.'">
             <input name="title" type="hidden" value="' .$attr['title'].'"/>
-            <input name="displayCount" type="hidden" value="' .$attr['display_count'].'"/>
+            <input name="displayCount" type="hidden" value="' .$attr['no_of_product'].'"/>
             <input name="layout" type="hidden" value="' .$attr['layout'].'"/>
             <input name="action" type="hidden" value="get_sw_product"/>
         </form>
--- src/data-feed-guide.php
+++ src/data-feed-guide.php
@@ -3,3 +3,16 @@
         http://wiki.affiliatewindow.com/index.php/Downloading_A_Feed
     </a>
 </h3>
+<h1>Shortcodes</h1>
+<ol>
+    <li>[SHOPWINDOW] - Default shortcode</li>
+</ol>
+<h2>Shortcode Options</h2>
+<ol>
+    <li>title='any title'</li>
+    <li>no_of_product=any number</li>
+</ol>
+<h2>Shortcode Examples</h2>
+<ol>
+    <li>[SHOPWINDOW title='hello world' no_of_product=2]</li>
+</ol>
