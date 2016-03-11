169d48cd8b720d264953baf230a5abac563ba85b
Muhammad Rahman <muhammad.rahman@affiliatewindow.com>
2016-03-11 00:01:21 +0000

    convert indent

--- src/Datafeed/PluginContainer.php
+++ src/Datafeed/PluginContainer.php
@@ -1,19 +1,20 @@
 <?php
 namespace Datafeed;
 
-class PluginContainer extends Pimple {
-	public function run()
-	{
-		foreach ( $this->values as $key => $content ) { // Loop on contents
-			$content = $this[ $key ];
+class PluginContainer extends Pimple
+{
+    public function run()
+    {
+        foreach ( $this->values as $key => $content ) { // Loop on contents
+            $content = $this[ $key ];
 
-			if ( is_object( $content ) ) {
-				$reflection = new \ReflectionClass( $content );
-				if ( $reflection->hasMethod( 'run' ) ) {
-					$content->run(); // Call run method on object
-				}
-			}
-		}
-	}
+            if ( is_object( $content ) ) {
+                $reflection = new \ReflectionClass( $content );
+                if ( $reflection->hasMethod( 'run' ) ) {
+                    $content->run(); // Call run method on object
+                }
+            }
+        }
+    }
 }
 
